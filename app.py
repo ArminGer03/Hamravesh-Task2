09512f8ff7e622473a8181846da0fe841174d4e4
ArminGer03 <a1382.geramirad@gmail.com>
2023-08-19 13:37:51 +0330

    Added random feature on existing webpage

--- app.py
+++ app.py
@@ -1,22 +1,62 @@
-import http.server
-import socketserver
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import random
 
-class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/random.html':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            with open('random.html', 'r') as file:
+                html_content = file.read()
+
+            self.wfile.write(html_content.encode())
+        elif self.path == '/logo.png':
+            self.send_response(200)
+            self.send_header('Content-type', 'image/png')
+            self.end_headers()
+
+            with open('logo.png', 'rb') as file:
+                image_data = file.read()
+
+            self.wfile.write(image_data)
+        elif self.path == '/styles.css':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/css')
+            self.end_headers()
+
+            with open('styles.css', 'r') as file:
+                css_content = file.read()
+
+            self.wfile.write(css_content.encode())
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'404 - Not Found')
+
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length).decode('utf-8')
-        start = int(post_data.split('&')[0].split('=')[1])
-        end = int(post_data.split('&')[1].split('=')[1])
-        result = random.randint(start, end)
+        post_data = self.rfile.read(content_length).decode()
+        num1 = int(post_data.split('&')[0].split('=')[1])
+        num2 = int(post_data.split('&')[1].split('=')[1])
+        random_number = random.randint(num1, num2)
+
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(str(result).encode('utf-8'))
 
-if __name__ == '__main__':
-    PORT = 8000
-    with socketserver.TCPServer(("", PORT), MyRequestHandler) as httpd:
-        print(f"Server running on port {PORT}")
-        httpd.serve_forever()
+        with open('random.html', 'r') as file:
+            html_content = file.read()
 
+        html_content = html_content.replace('{random_number}', str(random_number))
+        self.wfile.write(html_content.encode())
+
+# Run the server
+def run():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, RequestHandler)
+    print('Starting server...')
+    httpd.serve_forever()
+
+run()
